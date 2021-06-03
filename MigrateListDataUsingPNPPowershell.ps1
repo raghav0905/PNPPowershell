cdcdc4821fc1d298c46f634a88606325b1b115da
raghav0905 <64787405+raghav0905@users.noreply.github.com>
2021-06-03 13:35:55 +0530

    Add files via upload

---
+++ MigrateListDataUsingPNPPowershell.ps1
@@ -0,0 +1,25 @@
+﻿
+#variables
+$sourceURL = "https://xyz.sharepoint.com/sites/abc"
+$listTitle = "Issue Tracker"
+$outputTemplateFile = "C:\Temp\ListSchema.xml"
+$destnURL = "https://lntinfotech.sharepoint.com/sites/DeveloperSiteDZ/TestTrainingSite"
+
+#Connect with source site
+Connect-PnPOnline -Url $sourceURL -SPOManagementShell
+ 
+#Get the template of source site lists
+$template = Get-PnPSiteTemplate -Handlers Lists -Out $outputTemplateFile -Force
+
+$listURL = "Lists/" + $listTitle
+
+#Add data to list 
+Add-PnPDataRowsToSiteTemplate -Path $outputTemplateFile -List $listURL
+
+Disconnect-PnPOnline
+
+#Connect to destination site
+Connect-PnPOnline -Url $destnURL -SPOManagementShell
+
+#Apply the site template to create list with data
+Invoke-PnPSiteTemplate -Path $outputTemplateFile
